096dcbb37a7d844e24cc41865b0a2637c048d776
Liyun Dai <dlyun2009@gmail.com>
2018-10-09 14:29:11 +0800

    添加缩放图片效果

--- changecfg/src/main/java/com/example/changecfg/MainActivity.java
+++ changecfg/src/main/java/com/example/changecfg/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.changecfg;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt=(Button) findViewById(R.id.bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Configuration config= getResources().getConfiguration();
+
+                if(config.orientation==Configuration.ORIENTATION_LANDSCAPE){
+                    MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                }
+                else{
+                    MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                }
+            }
+        });
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        Toast.makeText(this, "sssssssss11111111111111111111" +
+                "sss333333333333333333333ssss", Toast.LENGTH_LONG).show();
     }
 }
